3737c68c233f6dba00c8f73af697f296594d4956
sharkcommits <sharkcommits@protonmail.com>
2023-09-29 00:18:33 +0300

    Multiprocessing support.

--- extractipedia.py
+++ extractipedia.py
@@ -1,219 +1,178 @@
-import pandas as pd
-import xml.dom.minidom
-import re
-import concurrent.futures
-from multiprocessing import get_context, cpu_count, Process, Pool
-import logging
-logging.getLogger().setLevel(logging.INFO)
+import queue
+from xml.etree.ElementTree import iterparse
+from util import *
 from time import perf_counter
-import os
-from random import sample
+from gc import collect
+import multiprocessing
+from multiprocessing import cpu_count
 
-import string
+WIKI_FILE = 'enwiki-20230901-pages-articles-multistream11.xml-p6899367p7054859'
+ANOTHER_WIKI = 'enwiki-20230920-pages-articles-multistream9.xml-p2936261p4045402'
 
-#Path to the file.
-input_file = 'enwiki-20230901-pages-articles-multistream11.xml-p6899367p7054859'
+database_file = 'wikimedia.db'
+table_name = 'pages'
 
-elements = xml.dom.minidom.parse(input_file).documentElement.getElementsByTagName('page')
+class WikiText:
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-default_cpu_count = cpu_count()
-IGNORE_THE_EXTRA_CONTENT = ['[[File:', '[[Category:', '[[Image:']
-IGNORE_SECTION = ['reference', 'references',
-                      'see also',
-                      'completed',
-                      'track listing',
-                      'gallery']
-IGNORE_REDIRECTS = ['#REDIRECT', '#redirect', '#Redirect', 'may refer to']
-ENTITIES = ['&nbsp;', '&lt;', '&gt;', '&amp;', '&quot;',	
-'&apos;', '&cent;', '&pound;', '&yen;' '&euro;', '&copy;', '&reg;']
-#OUTPUT FILES
-output_files = list(string.ascii_lowercase) + [str(number) for number in range(0,10)]
+    def __init__(self, file, batch_size):
+        self.totalCount = 0
+        self.articleCount = 0
+        self.redirectCount = 0
+        self.file = file
+        self.batch_size = batch_size
 
-def split_page(chunk=False):
-    #Splits the entire page into smaller pages and updates the self.extraction (dict).
-    extraction = {}
-    """
-    :params chunk (bool): For debugging purpose. False (default).
-    """
-    #Empty the dict in case there is any data.
-    CHUNK_SIZE = 50
-    page_count = 0
-    if chunk:
-        _ = sample(elements, CHUNK_SIZE)
-    else:
-        _ = elements
-    for element in _:
-        title = element.getElementsByTagName('title')[0].childNodes[0].nodeValue
-        text = element.getElementsByTagName('text')[0].childNodes[0].nodeValue
-        ns = element.getElementsByTagName('ns')[0].childNodes[0].nodeValue
-        id = element.getElementsByTagName('id')[0].childNodes[0].nodeValue
-        if any(redirect in text for redirect in IGNORE_REDIRECTS):
-            continue
-        else:
-            #Get the text, nothing else.
-            if ns == '0':
-                extraction.update({id:[title, text]})
-                page_count += 1
-    logging.info(f'Split {page_count} pages!')
-    return extraction
+        self.current_batch = []
+        self.current_batch_size = 0
 
-def cleaning_text(chunk):
+        self.xml_parser = None
 
-    beginning = perf_counter()
+    def tag(self, string):
+        idx = k = string.rfind('}')
+        if idx != -1:
+            string = string[idx + 1:]
+        return string 
+    
+    def __iter__(self):
+        return self
 
-    plain_text = {}
+    def __next__(self):
 
-    """
-    :params chunk (bool): If True, randomly extracts pages by chunk size. For debugging purposes. (default: False)
-    """
+        is_first = True
 
-    extraction = split_page(chunk)
-    ids, titles, texts = ([] for x in range(3))
-   
-    ids = list(extraction.keys())[:]
-    titles = [d[0] for d in extraction.values()][:]
-    texts = [d[1] for d in extraction.values()][:]
+        if self.xml_parser is None:
+            self.xml_parser = iterparse(self.file, events=("start", "end"))
 
-    for id, title, text in zip(ids, titles, texts):
-        #We're gonna remove the HTML-like tags.
-        text = re.sub("<!-+(.*?)-+>", '', text, flags=re.DOTALL)
-        #Remove the part after 'IGNORE_SECTION'.
-        _id = [text.find(part) for part in re.findall(r'=+(.*?)=+', text) if part.lower().strip() in IGNORE_SECTION]
-        text = text[:min(_id)] if len(_id) != 0 else text
+        for event, element in self.xml_parser:
+            tagName = self.tag(element.tag)
 
-        # ==CURLY BRACKET REMOVER== # 
-        def remove_nested_curly(text):
-            stringus = ''
-            bumble = 0
-            for i in text:
-                if i == '{':
-                    bumble += 1
-                elif i == '}'and bumble > 0:
-                    bumble -= 1
-                elif bumble == 0:
-                    stringus += i
-            return stringus
-        text = remove_nested_curly(text)
-        #Remove HTML-like tag pairs.
-        text = re.sub("<(.*?)>(.*?)<\/(.*?)>", '', text, flags=re.MULTILINE)
-        text = re.sub(r'<(.*?)>', '', text)
+            if is_first:
+                is_first = False
+            if event == 'start':
+                if tagName == 'page':
+                    title = ''
+                    id = -1
+                    redirect = ''
+                    inrevision = False
+                    ns = 0
+                elif tagName == 'revision':
+                    inrevision = True
+            else:
+                
+                if tagName == 'title':
+                    title = element.text   
+                elif tagName == 'text':
+                    text = element.text
+                elif tagName == 'id' and not inrevision:
+                    id = int(element.text)
+                elif tagName == 'redirect':
+                    redirect = element.attrib['title']
+                elif tagName == 'ns':
+                    ns = int(element.text)
+                elif tagName == 'page':
+                    self.totalCount += 1 
+                    try:
+                        if ns == 0:
+                            if len(redirect) > 0:
+                                self.redirectCount += 1
+                            else:
+                                if 'may refer to' in text:
+                                    pass
+                                else:
+                                    self.articleCount += 1
+                                    TEMP_DICT = {title:[id, text]}
+                                    self.current_batch.append(TEMP_DICT)
+                                    self.current_batch_size += 1
+                                    del TEMP_DICT 
+                    except Exception as e:
+                        print(e) 
+
+                    title = ''
+                    redirect = ''
+                    text = ''
+                    ns = -1      
+                    element.clear() 
+
+                if self.current_batch_size >= self.batch_size:
+                    # Yield the current batch as a single string
+                    batch_to_yield = self.current_batch.copy()
+                    self.current_batch.clear()
+                    _ = collect()
+                    self.current_batch_size = 0
+                    return batch_to_yield
+
+
+        # Yield any remaining data in the last batch
+        if self.current_batch:
+            batch_to_yield = self.current_batch.copy()
+            self.current_batch.clear()
+            _ = collect()
+            self.current_batch_size = 0
+            return batch_to_yield
         
-        #Checks the page and removes extra contents such as files and categories.
-        for extra in IGNORE_THE_EXTRA_CONTENT:
-            _ = 0
-            _start = []
-            _end = []
-            while _ < len(text):
-                index = text.find(extra, _)
-                if index == -1:
-                    break
-                _start.append(index)
-                _ = index + 1
-            for idx in _start:
-                idx += 2
-                open_bracket_count = 2
-                for _letter in text[idx:]:
-                    if open_bracket_count != 0:
-                        if _letter == ']':
-                            open_bracket_count -= 1
-                        elif _letter == '[':
-                            open_bracket_count += 1
-                        else:
-                            pass
-                        idx += 1
-                    else:
-                        break
-                _end.append(idx)
-            #Reversed lists to not cause errors during indexing.    
-            reversed_start = _start[::-1]
-            reversed_end = _end[::-1]
-            for start, end in zip(reversed_start, reversed_end):
-                sub = text[start:end]
-                text = text.replace(sub, '')
-        
-        #We're accessing the data between the double brackets and check if it's useful for us or not. (Alias Remover)
-        for part in re.findall(r'\[\[.+?\]\]', text, flags=re.DOTALL):
-            if '|' in part:
-                second = part.split('|')[1].strip(']')
-                text = text.replace(part, second)
-        
-        #Then we're gonna remove all the links in the text.
-        text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
-        
-        #Remove headlines, we don't need them.
-        text = re.sub(r'=+(.*?)=+', '', text)
-        #Remove the special characters, not cool.    
-        for entity in ENTITIES:
-            special = re.compile(entity)
-            text = re.sub(special, '', text)
-        #Remove the symbols we don't need.
-        symbols = ["'", "{", "}", "*", '#', '"', "[", "]"] #() EKLE
-        """
-        for symbol in symbols:
-            text = text.replace(symbol, '')
-        """
-        text.translate({ord(ch):'' for ch in symbols}) #NEW
-        text = text.replace('()', '') #NEW
-        #Some additional regex for wikipedia format.
-        text = re.sub(r'\n\|.*\n', "", text)
-        text = re.sub(r'\|+(.*?)\|+', "", text)
-        three = re.escape('align=')+r'.*\n'
-        text = re.sub(three, "", text)
-        
-        #New line and space remover, the data must be plain text.
-        text = re.sub(r'\n+', ' ', text)
-        text = re.sub(r'\s\s+', ' ', text)
-        #Removing the meaningless tags.
-        text = text.replace('&ndash;', '-')
-        
-        #Removing the trailing spaces, if there is any.
-        text = text.lstrip(' ').rstrip(' ')
-        title = title
-        plain_text.update({id:[title,text]})
+        raise StopIteration
 
-    finish = perf_counter()    
-    logging.info('Cleaning process took only %d seconds. Not fast, huh?', finish - beginning) 
+def process_batch(batch):
+    result = cleaning_text(batch)
+    update_sqlite_table_with_dict(database_file, table_name, result)
 
-    ###### SECOND PART ######
+class BatchProcessor:
+    def __init__(self, max_queue_size, process_func, num_workers):
+        self.max_queue_size = max_queue_size
+        self.batch_queue = multiprocessing.Queue(maxsize=max_queue_size)
+        self.process_func = process_func
+        self.num_workers = num_workers
+        self.stop_event = multiprocessing.Event()
 
-    root_path = 'bundle'
-    output_path = os.path.join(dir_path, root_path)
-    if root_path not in os.listdir(dir_path):
-        os.makedirs(output_path)
+    def process_batches(self, data_generator):
+        producer_process = multiprocessing.Process(target=self._produce_batches, args=(data_generator,))
+        consumer_processes = [multiprocessing.Process(target=self._consume_batches) for _ in range(self.num_workers)]
 
-    for letter in output_files:
+        producer_process.start()
+        for consumer_process in consumer_processes:
+            consumer_process.start()
 
-        path_parquet = output_path + '/' + letter + '.parquet.gzip'
-        print("PLAIN TEXT LENGTH: ", len(plain_text))
-        _ids = list(plain_text.keys())[:]
-        _titles = [d[0] for d in plain_text.values()]
-        _texts = [d[1] for d in plain_text.values()]
-        id_list = [y for x, y in zip(_titles, _ids) if x[0].lower() == letter]
+        producer_process.join()
+        for consumer_process in consumer_processes:
+            consumer_process.join()
 
-        TEMP_DICT = {}
-        
-        for di, dt, de in zip(_ids, _titles, _texts):
-            if di in id_list:
-                TEMP_DICT.update({di: [dt, de]})
-        TEMP_DF = pd.DataFrame.from_dict(TEMP_DICT, orient='index', columns=['title', 'text'])    
-        
-        TEMP_DF.to_parquet(path_parquet, compression='gzip')
-        
-        logging.info(f'{letter}/Dump was successful to {output_path}')
+    def _produce_batches(self, data_generator):
+        for item in data_generator:
+            self.batch_queue.put(item)
 
-        plain_text = {id:[title, text] for id, [title, text] in plain_text.items() if id not in id_list}
+        # Signal that there are no more items to be added to the queue
+        for _ in range(self.num_workers):
+            self.batch_queue.put(None)
 
-def main():
+    def _consume_batches(self):
+        while not self.stop_event.is_set():
+            try:
+                batch = self.batch_queue.get(timeout=1)
+                if batch is None:
+                    break  # Exit the loop when sentinel value is received
 
-    main_start = perf_counter()
+                # Process the batch using the provided function
+                self.process_func(batch)
+            except queue.Empty:
+                pass
+            except Exception as e:
+                print(f"Error: {e}")
 
-    cleaning_text(chunk=False)
+    def stop(self):
+        self.stop_event.set()
 
-    main_end = perf_counter()
+if __name__ == "__main__":
 
-    print(f'Main Process took {main_end-main_start} seconds.')    
+    start_time = perf_counter()
+    num_workers = cpu_count() - 2
+    max_queue_size = 8
 
-if __name__ == '__main__':
+    batch_processor = BatchProcessor(max_queue_size, process_func=process_batch, num_workers=num_workers)
 
-    main()     
+    batch_size = 1500
+    generator = WikiText(ANOTHER_WIKI, batch_size)
+
+    batch_processor.process_batches(generator)
+
+    bed_time = perf_counter()
+
+    print(f'It took exactly {bed_time - start_time} seconds.')
---
+++ util.py
@@ -0,0 +1,212 @@
+import os
+import re
+import json
+import pickle
+import sqlite3
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
+ENTITIES = ['&nbsp;', '&lt;', '&gt;', '&amp;', '&quot;',	
+'&apos;', '&cent;', '&pound;', '&yen;' '&euro;', '&copy;', '&reg;']
+
+IGNORE_SECTION = ['reference', 'references', 'see also', 'completed',
+                  'track listing', 'gallery', '<gallery>']
+
+IGNORE_THE_EXTRA_CONTENT = ['[[File:', '[[Category:', '[[Image:']
+
+#Checks the object if it's picklable or not. (:_:DEBUGGING PURPOSE:_:)
+def is_picklable(obj):
+    try:
+        pickle.dumps(obj)
+        return True
+    except (pickle.PicklingError, TypeError):
+        return False
+    
+def table_exists(cursor, table_name):
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
+    return cursor.fetchone() is not None    
+    
+def update_sqlite_table_with_dict(database_file, table_name, data_dict):
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect(database_file)
+        cursor = conn.cursor()
+
+        # Check if the table exists
+        if not table_exists(cursor, table_name):
+            # If the table doesn't exist, create it
+            create_table_sql = f"CREATE TABLE {table_name} (key TEXT PRIMARY KEY, value TEXT);"
+            cursor.execute(create_table_sql)
+        
+        # Iterate through the dictionary and update the database
+        for key, value_list in data_dict.items():
+            # Serialize the list to JSON before inserting
+            value_json = json.dumps(value_list)
+
+            # Define the SQL update statement
+            update_sql = f"INSERT OR REPLACE INTO {table_name} (key, value) VALUES (?, ?)"
+            
+            # Execute the SQL statement with the current key and serialized value
+            cursor.execute(update_sql, (key, value_json))
+        
+        # Commit the changes to the database
+        conn.commit()
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+        print("Data updated successfully.")
+    
+    except sqlite3.Error as e:
+        print(f"SQLite error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")    
+    
+def retrieve_data_from_sqlite(database_file, table_name):
+    data_dict = {}
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect(database_file)
+        cursor = conn.cursor()
+
+        # Retrieve data from the database
+        cursor.execute(f"SELECT key, value FROM {table_name}")
+        for key, value_json in cursor.fetchall():
+            # Deserialize the JSON back to a list
+            value_list = json.loads(value_json)
+            data_dict[key] = value_list
+        
+        print("Data retrieved successfully.")
+    
+    except sqlite3.Error as e:
+        print(f"SQLite error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # Close the cursor and connection in the finally block
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+    return data_dict    
+
+def cleaning_text(list_of_dicts):
+
+    try:
+
+        plain_text = {}
+
+        def remove_nested_curly(text):
+            stringus = ''
+            bumble = 0
+            for i in text:
+                if i == '{':
+                    bumble += 1
+                elif i == '}'and bumble > 0:
+                    bumble -= 1
+                elif bumble == 0:
+                    stringus += i
+            return stringus
+
+        """
+        :params chunk (bool): If True, randomly extracts pages by chunk size. For debugging purposes. (default: False)
+        """
+        titles, ids, texts = ([] for x in range(3))
+
+        titles = [key for d in list_of_dicts for key in d.keys()]
+        ids = [value[0] for d in list_of_dicts for value in d.values()]
+        texts = [value[1] for d in list_of_dicts for value in d.values()]
+
+
+        for title, id, text in zip(titles, ids, texts):
+            #We're gonna remove the HTML-like tags.
+            text = re.sub("<!-+(.*?)-+>", '', text, flags=re.DOTALL)
+            #Remove the part after 'IGNORE_SECTION'.
+            _id = [text.find(part) for part in re.findall(r'=+(.*?)=+', text) if part.lower().strip() in IGNORE_SECTION]
+            text = text[:min(_id)] if len(_id) != 0 else text
+
+            # ==CURLY BRACKET REMOVER== # 
+
+            text = remove_nested_curly(text)
+            #Remove HTML-like tag pairs.
+            text = re.sub("<(.*?)>(.*?)<\/(.*?)>", '', text, flags=re.MULTILINE)
+            text = re.sub(r'<(.*?)>', '', text)
+            
+            #Checks the page and removes extra contents such as files and categories.
+            for extra in IGNORE_THE_EXTRA_CONTENT:
+                _ = 0
+                _start = []
+                _end = []
+                while _ < len(text):
+                    index = text.find(extra, _)
+                    if index == -1:
+                        break
+                    _start.append(index)
+                    _ = index + 1
+                for idx in _start:
+                    idx += 2
+                    open_bracket_count = 2
+                    for _letter in text[idx:]:
+                        if open_bracket_count != 0:
+                            if _letter == ']':
+                                open_bracket_count -= 1
+                            elif _letter == '[':
+                                open_bracket_count += 1
+                            else:
+                                pass
+                            idx += 1
+                        else:
+                            break
+                    _end.append(idx)
+                #Reversed lists to not cause errors during indexing.    
+                reversed_start = _start[::-1]
+                reversed_end = _end[::-1]
+                for start, end in zip(reversed_start, reversed_end):
+                    sub = text[start:end]
+                    text = text.replace(sub, '')
+            
+            #We're accessing the data between the double brackets and check if it's useful for us or not. (Alias Remover)
+            for part in re.findall(r'\[\[.+?\]\]', text, flags=re.DOTALL):
+                if '|' in part:
+                    second = part.split('|')[1].strip(']')
+                    text = text.replace(part, second)
+            
+            #Then we're gonna remove all the links in the text.
+            text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+            
+            #Remove headlines, we don't need them.
+            text = re.sub(r'=+(.*?)=+', '', text)
+            #Remove the special characters, not cool.    
+            for entity in ENTITIES:
+                special = re.compile(entity)
+                text = re.sub(special, '', text)
+            #Remove the symbols we don't need.
+            symbols = ["'", "{", "}", "*", '#', '"', "[", "]"] #() EKLE
+            """
+            for symbol in symbols:
+                text = text.replace(symbol, '')
+            """
+            text = text.translate({ord(ch):'' for ch in symbols}) #NEW
+            text = text.replace('()', '') #NEW
+            #Some additional regex for wikipedia format.
+            text = re.sub(r'\n\|.*\n', "", text)
+            text = re.sub(r'\|+(.*?)\|+', "", text)
+            three = re.escape('align=')+r'.*\n'
+            text = re.sub(three, "", text)
+            
+            #New line and space remover, the data must be plain text.
+            text = re.sub(r'\n+', ' ', text)
+            text = re.sub(r'\s\s+', ' ', text)
+            #Removing the meaningless tags.
+            text = text.replace('&ndash;', '-')
+            
+            #Removing the trailing spaces, if there is any.
+            text = text.lstrip(' ').rstrip(' ')
+            title = title
+            plain_text.update({title:[id,text]})
+
+        return plain_text    
+    except Exception as e:
+        print(e) 
